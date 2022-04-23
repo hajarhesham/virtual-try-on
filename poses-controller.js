d4601279713ffa02005dd2783bdaaf81c4e103dc
Mohamed Fathy <mofasa.peace@gmail.com>
2022-04-23 16:23:49 +0200

    removed duplicates

--- poses-controller.js
+++ poses-controller.js
@@ -2,14 +2,12 @@ const asyncHandler = require('express-async-handler')
 const Pose = require('./models/poseModel')
 
 // get all poses
-// working
 const getPoses = asyncHandler(async (req, res) => {
     const poses = await Pose.find();
     res.status(200).send(poses);
 });
 
 // get pose by id
-// working
 const getPoseById = asyncHandler(async (req, res) => {
     const id = req.params.poseId;
     const pose = await Pose.findById(id);
@@ -21,7 +19,6 @@ const getPoseById = asyncHandler(async (req, res) => {
 });
 
 // get theta by id
-// working
 const getThetaById = asyncHandler(async (req, res) => {
     const id = req.params.poseId;
     const pose = await Pose.findById(id);
@@ -33,7 +30,6 @@ const getThetaById = asyncHandler(async (req, res) => {
 });
 
 // get pose name
-// working
 const getNameById = asyncHandler(async (req, res) => {
     const id = req.params.poseId;
     const pose = await Pose.findById(id);
@@ -45,7 +41,6 @@ const getNameById = asyncHandler(async (req, res) => {
 });
 
 // add new pose
-// working
 const createPoseHandler = asyncHandler(async (req, res) => {
     const body = req.body;
     const response = await Pose.create(body)
@@ -53,13 +48,11 @@ const createPoseHandler = asyncHandler(async (req, res) => {
 });
 
 // delete pose
-// working
 const deleteClientPoseById = asyncHandler(async (req, res) => {
     const poseId = req.params.poseId;
     const pose = await Pose.deleteOne({_id: poseId});
     res.send(pose)
 });
-
 
 module.exports = {
     getPoses,
@@ -69,4 +62,3 @@ module.exports = {
     createPoseHandler,
     deleteClientPoseById
 };
-
